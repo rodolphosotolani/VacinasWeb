a2d1fa825c748ae5d3262e472e6a3c7c8ff8f102
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2019-08-19 21:47:56 -0300

    add gitignore

--- src/main/java/br/com/sotolani/vacinasweb/controller/UsuarioController.java
+++ src/main/java/br/com/sotolani/vacinasweb/controller/UsuarioController.java
@@ -3,19 +3,27 @@ package br.com.sotolani.vacinasweb.controller;
 import br.com.sotolani.vacinasweb.controller.dto.UsuarioDTO;
 import br.com.sotolani.vacinasweb.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping
 public class UsuarioController {
 
     @Autowired
     private UsuarioService service;
 
-    @RequestMapping("/usuarios")
-    public List<UsuarioDTO> getAll() {
+    @GetMapping("/usuarios")
+    public List<UsuarioDTO> findAll() {
         return UsuarioDTO.converter(this.service.findAll());
     }
+
+    @GetMapping("/usuarios")
+    public UsuarioDTO findByLogin(String login) {
+        return UsuarioDTO.converter(service.findByLogin(login));
+    }
 }
+
--- src/main/java/br/com/sotolani/vacinasweb/controller/dto/UsuarioDTO.java
+++ src/main/java/br/com/sotolani/vacinasweb/controller/dto/UsuarioDTO.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Data
 @Builder
 @NoArgsConstructor
